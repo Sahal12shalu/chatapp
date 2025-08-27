0d34f89bbe8d1aede8a75408124619c35cde0022
muhammed sahal <sahalshalu806@gmail.com>
2025-08-27 05:11:09 -0400

    updated

--- src/app/Component/ui/file-upload.jsx
+++ src/app/Component/ui/file-upload.jsx
@@ -44,7 +44,7 @@ export const FileUpload = ({
     const {data : session , status} = useSession()
 
     const formData = {
-        id: session.user.id ,
+        id: session.user.id || '' ,
         image: 'A'
     }
 
